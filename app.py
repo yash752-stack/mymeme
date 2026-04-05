bbf8e50bef4a21e9fe94c812175ca9543ac457b1
Yash Chaudhary <yash@mymeme.com>
2026-04-05 20:25:13 +0000

    💀 yashonlyfans

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="goon", page_icon="😼", layout="centered")
+st.set_page_config(page_title="yashonlyfans", page_icon="😼", layout="centered")
 
 st.image("cat_meme.jpg", use_container_width=True)
